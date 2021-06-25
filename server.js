3488041ef248b4d2d3f3764ea62c97fdba323754
Lari Souza <larissaccsouza@gmail.com>
2021-06-25 20:05:15 -0300

    adiciona código servidor

---
+++ server.js
@@ -0,0 +1,26 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+
+const estados = require("./data/estados-cidades.json")
+
+app.get("/",(request,response) => {
+    response.status(200).send("Testando homework! <3")
+})
+
+app.get("/estados/todos",(request,response) => {  
+    response.status(200).send(estados)     
+})
+
+app.get("/estados/:sigla",(request,response) => {
+    const estadoSigla = request.params.sigla
+    console.log(estadoSigla)
+    console.log(require.params)
+
+    response.status(200).send(estados.find(siglas => siglas.sigla === estadoSigla))
+
+})
+
+app.listen(8080, () => {
+    console.log("Servidor rodando na porta 8080")
+}) 
